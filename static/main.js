6eecd5f30ad0935afba93b76e1f698e3bdc172ee
ShoshinNikita <shoshin.nikita@gmail.com>
2018-05-13 01:40:33 +0300

    Work version of interface

--- static/main.js
+++ static/main.js
@@ -1,29 +1,42 @@
 const buttonTemplate = `
 <div>
 	<input type="button" class="db_button" value="{0}" onclick="ChooseDB('{1}')" title="Choose">
-	<input type="button" value="X" style="float: right; margin-right: 1vw; height: 4vh; width: 15%;" onclick="CloseDB('{1}')" title="Close">
-</div>`
+	<i class="material-icons btn" style="float: right; margin-right: 1vw; font-size: 30px !important;" title="Close" onclick="CloseDB('{1}')">close<\/i>
+	<br>
+	<br>
+<\/div>`
 
 const recordTemplate = `
 <div>
-	<i class="material-icons" icon>assignment</i>
-	{0}: {1}
-</div>
+	<i class="material-icons" icon>assignment<\/i>
+	<span class="record" onclick="ShowFullRecord({0});"><b>{1}<\/b>:<\/span> {2}
+<\/div>
 `
 const bucketTemplate = `
 <div>
-	<i class="material-icons" icon>folder</i>
-	<span onclick="Next(currentDBPath, '{0}');">{0}</span>
-</div>
+	<i class="material-icons" icon>folder<\/i>
+	<span class="bucket" onclick="Next(currentDBPath, '{0}');"><b>{0}<\/b><\/span>
+<\/div>
 `
 
 const backButton = `
 <div>
-	<i class="material-icons" icon onclick="Back(currentDBPath);">more_horiz</i>
-</div>
+	<i class="material-icons btn" icon onclick="Back(currentDBPath);" title="Back">more_horiz<\/i>
+<\/div>
+`
+
+const fullRecordTemplate = `
+<div>
+	<b>Key:<\/b> {0}
+<\/div>
+<br>
+<div>
+	<b>Value:<\/b> {1}
+<\/div>
 `
 
 var currentDBPath = ""
+var currentData = null
 
 function OpenDB() {
 	var path = prompt("Please, enter the path to the database")
@@ -35,6 +48,30 @@ function OpenDB() {
 		},
 		success: function(result){
 			result = JSON.parse(result);
+			ShowDBList()
+		},
+		error: function(result) {
+			console.log(result.responseText)
+		}
+	})
+}
+
+function CloseDB(path) {
+	$.ajax({
+		url: "/api/closeDB",
+		type: "POST",
+		data: {
+			"path": path,
+		},
+		success: function(result){
+			if (path == currentDBPath) {
+				$("#dbName").html("<i>Name:<\/i> ?")
+				$("#dbPath").html("<i>Path:<\/i> ?")
+				$("#dbSize").html("<i>Size:<\/i> ?")
+				$("#db_tree").html("")
+				$("#record_data").html("")
+				currentDBPath = ""
+			}
 			ShowDBList()
 		},
 		error: function(result) {
@@ -62,16 +99,21 @@ function ShowDBList() {
 	})
 }
 
-function CloseDB(path) {
+function ChooseDB(path) {
+	currentDBPath = path
 	$.ajax({
-		url: "/api/closeDB",
-		type: "POST",
+		url: "/api/current",
+		type: "GET",
 		data: {
 			"path": path,
 		},
 		success: function(result){
-			console.log(result.responseText)
-			ShowDBList()
+			result = JSON.parse(result)
+			$("#dbName").html("<i>Name:<\/i> " + result.name)
+			$("#dbPath").html("<i>Path:<\/i> " + result.path)
+			$("#dbSize").html("<i>Size:<\/i> " + result.size + " Kb")
+			$("#record_data").html("")
+			ShowTree(result)
 		},
 		error: function(result) {
 			console.log(result.responseText)
@@ -82,18 +124,24 @@ function CloseDB(path) {
 function ShowTree(data) {
 	var result = ""
 	if (data.canBack) {
-		result = backButton
+		result = backButton;
 	}
 	
-	var records = data.records
+	var records = data.records;
+	currentData = records;
 	for (i in records) {
 		if (records[i].type == "bucket") {
-			result += bucketTemplate.format(records[i].key)
+			result += bucketTemplate.format(records[i].key);
 		} else if (records[i].type == "record") {
-			result += recordTemplate.format(records[i].key, records[i].value)
+			var value = records[i].value;
+			if (value.length > 40) {
+				value = value.substring(0, 60);
+				value += "..."
+			}
+			result += recordTemplate.format(i, records[i].key, value);
 		}
 	}
-	$("#db_tree").html(result)
+	$("#db_tree").html(result);
 }
 
 function Next(path, bucket) {
@@ -106,6 +154,7 @@ function Next(path, bucket) {
 		},
 		success: function(result){
 			result = JSON.parse(result)
+			$("#record_data").html("")
 			ShowTree(result)
 		},
 		error: function(result) {
@@ -123,6 +172,7 @@ function Back(path) {
 		},
 		success: function(result){
 			result = JSON.parse(result)
+			$("#record_data").html("")
 			ShowTree(result)
 		},
 		error: function(result) {
@@ -131,27 +181,11 @@ function Back(path) {
 	})
 }
 
-function ChooseDB(path) {
-	currentDBPath = path
-	$.ajax({
-		url: "/api/current",
-		type: "GET",
-		data: {
-			"path": path,
-		},
-		success: function(result){
-			result = JSON.parse(result)
-			$("#dbName").html("Name: " + result.name)
-			$("#dbPath").html("Path: " + result.path)
-			$("#dbSize").html("Size: " + result.size + " Kb")
-			ShowTree(result)
-		},
-		error: function(result) {
-			console.log(result.responseText)
-		}
-	})
+function ShowFullRecord(number) {
+	console.log(currentData[number])
+	var result = fullRecordTemplate.format(currentData[number].key, currentData[number].value)
+	$("#record_data").html(result)
 }
-
 
 String.prototype.format = function () {
 	var a = this;
