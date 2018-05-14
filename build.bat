3b73f7c504c163edabc1cb450dc52c1ea9be9541
ShoshinNikita <shoshin.nikita@gmail.com>
2018-05-14 20:23:16 +0300

    Add archive with compiled program

--- build.bat
+++ build.bat
@@ -7,10 +7,10 @@ go vet converters db web
 SET /p command=Should continue(y/n): 
 IF "%command%" == "y" (
 	:: Build for windows
-	go build -o program/boltBrowser.exe src/main.go
+	go build -o program/boltBrowser_v1.0.exe src/main.go
 
 	:: Build for linux
 	SET GOOS=linux
 	SET GOARCH=amd64
-	go build -o program/boltBrowser src/main.go
+	go build -o program/boltBrowser_v1.0 src/main.go
 )
