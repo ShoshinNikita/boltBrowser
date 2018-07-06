97303361d2864010192b0ad244c31fa9c2612dd8
ShoshinNikita <shoshin.nikita@gmail.com>
2018-07-07 01:01:53 +0300

    Add default tag #32
    Every field of Opts has tag "default". Function setDefaultValues() parses this tag and change value of the field.

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,43 +2,92 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"reflect"
+	"strconv"
 )
 
 var Opts struct {
 	// Port for website (with ':')
-	Port string
+	Port string `default:":500"`
 	// Debug mode
-	Debug bool
+	Debug bool `default:"false"`
 	// Offset - number of records on single screen
-	Offset int
+	Offset int `default:"100"`
 	// CheckVer - should the program check check is there a new version
-	CheckVer bool
+	CheckVer bool `default:"true"`
 	// IsWriteMode - can program edit databases
-	IsWriteMode bool
+	IsWriteMode bool `default:"true"`
 	// OpenBrowser - should the program open a browser automatically
-	OpenBrowser bool
+	OpenBrowser bool `default:"true"`
 	// NeatWindow - should the program open the special neat window
-	NeatWindow bool
+	NeatWindow bool `default:"true"`
 }
 
 // ParseConfig parses flags like -port, -debug, -offset and etc.
 // If there's no any flags, it tries to parse config file "config.ini"
 func ParseConfig() {
+	defer func() {
+		if r := recover(); r != nil {
+			exit(r)
+		}
+	}()
+
+	setDefaultValues()
+
 	if len(os.Args) > 1 {
 		parseFlags()
 	}
 }
 
+// setDefaultValues sets default values of Opts's fields.
+// If tag default was missed it panics
+// If type of field isn't [int, string, bool] it panics
+func setDefaultValues() {
+	// For change value
+	v := reflect.ValueOf(&Opts).Elem()
+	// For getting tags
+	t := v.Type()
+
+	// Opts is always struct, so we shouldn't check if tp.Kind() == reflect.Struct
+	for i := 0; i < v.NumField(); i++ {
+		f := v.Field(i)
+		def := t.Field(i).Tag.Get("default")
+		if def == "" {
+			panicf("default tag of field %s is empty", t.Field(i).Name)
+		}
+
+		switch f.Kind() {
+		case reflect.Bool:
+			b := func() bool {
+				if def == "true" {
+					return true
+				}
+				return false
+			}()
+			f.SetBool(b)
+		case reflect.String:
+			f.SetString(def)
+		case reflect.Int:
+			i, _ := strconv.ParseInt(def, 10, 64)
+			f.SetInt(i)
+		default:
+			panicf("Bad type of a field if Opts. Type: %s", f.Kind().String())
+		}
+	}
+}
+
 // parseFlags parses command line flags
 func parseFlags() {
-	flag.StringVar(&Opts.Port, "port", ":500", "port for website (with ':')")
-	flag.BoolVar(&Opts.Debug, "debug", false, "debug mode")
-	flag.IntVar(&Opts.Offset, "offset", 100, "number of records on single page")
-	flag.BoolVar(&Opts.CheckVer, "checkVer", true, "should program check is there a new version")
-	flag.BoolVar(&Opts.IsWriteMode, "writeMode", true, "can program edit dbs")
-	flag.BoolVar(&Opts.OpenBrowser, "openBrowser", true, "should the program open a browser automatically")
-	flag.BoolVar(&Opts.NeatWindow, "neatWindow", true, "should the program open a neat window")
+	// We can use fields of Opts as default, because we already set default values by calling of setDefaultValues()
+	flag.StringVar(&Opts.Port, "port", Opts.Port, "port for website (with ':')")
+	flag.BoolVar(&Opts.Debug, "debug", Opts.Debug, "debug mode")
+	flag.IntVar(&Opts.Offset, "offset", Opts.Offset, "number of records on single page")
+	flag.BoolVar(&Opts.CheckVer, "checkVer", Opts.CheckVer, "should program check is there a new version")
+	flag.BoolVar(&Opts.IsWriteMode, "writeMode", Opts.IsWriteMode, "can program edit dbs")
+	flag.BoolVar(&Opts.OpenBrowser, "openBrowser", Opts.OpenBrowser, "should the program open a browser automatically")
+	flag.BoolVar(&Opts.NeatWindow, "neatWindow", Opts.NeatWindow, "should the program open a neat window")
 	flag.Parse()
 
 	// Checking of ':' before port
@@ -47,3 +96,11 @@ func parseFlags() {
 	}
 }
 
+func panicf(format string, v ...interface{}) {
+	panic(fmt.Sprintf(format, v))
+}
+
+func exit(v ...interface{}) {
+	fmt.Println(v...)
+	os.Exit(2)
+}
